d05ad9fc48dd6bac1a008a10312631cacbc317bd
hadzi42 <hadzi42@gmail.com>
2023-06-21 18:29:03 +0200

    Fix duplicate AssemblyInfos in product dlls.

---
+++ Source/AsgardCore.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AsgardCore.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Hadzi Arts")]
+[assembly: AssemblyProduct("AsgardCore")]
+[assembly: AssemblyCopyright("Copyright © Hadzi Arts 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
